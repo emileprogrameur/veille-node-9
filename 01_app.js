91549a8ccf3019342f705288b1c694708d4a89f8
emileprogrameur <emile_bisaillon@hotmail.fr>
2018-02-16 13:22:38 -0500

    fonteawesome

--- 01_app.js
+++ 01_app.js
@@ -34,7 +34,7 @@ app.post('/ajouter', (req, res) => {
 let db // variable qui contiendra le lien sur la BD
 MongoClient.connect('mongodb://127.0.0.1:27017/carnet_adresse', (err, database) => {
 	if (err) return console.log(err)
-	db = database.db('carnet_adresse2')
+	db = database.db('carnet_adresse')
 	// lancement du serveur Express sur le port 8081
 	app.listen(8081, () => {
 	 	console.log('connexion à la BD et on écoute sur le port 8081')
